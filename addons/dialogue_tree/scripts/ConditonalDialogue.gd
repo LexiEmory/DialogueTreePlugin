aeb5548b0b3a3463945ea898186bc90197b5f02b
nitsuga2 <79530090+nitsuga2@users.noreply.github.com>
2021-07-23 21:10:26 -0300

    Update ConditonalDialogue.gd

--- addons/dialogue_tree/scripts/ConditonalDialogue.gd
+++ addons/dialogue_tree/scripts/ConditonalDialogue.gd
@@ -11,8 +11,8 @@ func save_data():
 	var dict = {
 		"filename" : get_filename(),
 		"name" : name,
-		"rect_x" : rect_position.x,
-		"rect_y" : rect_position.y,
+		"rect_x" : offset.x,
+		"rect_y" : offset.y,
 		"rect_size_x" : rect_size.x,
 		"rect_size_y" : rect_size.y,
 		"conditonal" : $Conditional.text
